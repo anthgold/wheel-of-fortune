3d010f3c3423a6f8d00bd4a143921552aa780642
Anthony Gold <anthgold@gmail.com>
2016-04-27 19:45:57 -0700

    first assertion error

--- src/test/java/WheelOfFortuneTest.java
+++ src/test/java/WheelOfFortuneTest.java
@@ -4,9 +4,9 @@ import static org.junit.Assert.*;
 public class WheelOfFortuneTest {
 
   @Test // nameOfFunction_testCase_expectedResult()
-  public void disemvoweler_oneConsonant_s() {
-    Disemvowel disemvowel = new Disemvowel();
-    assertEquals("s", disemvowel.disemvoweler("s"));
+  public void disemvoweler_oneConsonant_n() {
+    WheelOfFortune disemvowel = new WheelOfFortune();
+    assertEquals("n", disemvowel.disemvoweler("n"));
   }
 
 }
