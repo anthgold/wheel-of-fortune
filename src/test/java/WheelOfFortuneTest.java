0038b6cc6cc00e0fc9ccff49f5281a4ca340597e
Anthony Gold <anthgold@gmail.com>
2016-04-28 04:55:48 -0700

    adds layout.vtl and home.vtl

--- src/test/java/WheelOfFortuneTest.java
+++ src/test/java/WheelOfFortuneTest.java
@@ -6,7 +6,7 @@ public class WheelOfFortuneTest {
   @Test // nameOfFunction_testCase_expectedResult()
   public void disemvoweler_oneConsonant_n() {
     WheelOfFortune disemvowel = new WheelOfFortune();
-    assertEquals("n", disemvowel.disemvoweler("n"));
+    assertEquals("t", disemvowel.disemvoweler("t"));
   }
 
   @Test // nameOfFunction_testCase_expectedResult()
