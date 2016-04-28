d4806d8a81b66e98f1c1703eedff2451b184dc27
Anthony Gold <anthgold@gmail.com>
2016-04-27 20:10:27 -0700

    tests one and two words

--- src/test/java/WheelOfFortuneTest.java
+++ src/test/java/WheelOfFortuneTest.java
@@ -9,6 +9,22 @@ public class WheelOfFortuneTest {
     assertEquals("n", disemvowel.disemvoweler("n"));
   }
 
+  @Test // nameOfFunction_testCase_expectedResult()
+  public void disemvoweler_oneVowel_dash() {
+    WheelOfFortune disemvowel = new WheelOfFortune();
+    assertEquals("-", disemvowel.disemvoweler("a"));
+  }
 
+  @Test // nameOfFunction_testCase_expectedResult()
+  public void disemvoweler_oneWord_cDashT() {
+    WheelOfFortune disemvowel = new WheelOfFortune();
+    assertEquals("c-t", disemvowel.disemvoweler("cat"));
+  }
+
+  @Test // nameOfFunction_testCase_expectedResult()
+  public void disemvoweler_twoWords_bLDashCK_cDashT() {
+    WheelOfFortune disemvowel = new WheelOfFortune();
+    assertEquals("bl-ck c-t", disemvowel.disemvoweler("black cat"));
+  }
 
 }
